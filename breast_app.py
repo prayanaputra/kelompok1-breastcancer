c0f0011153ab794ca18e84d4fe2fd77350166fc0
prayanaputra <108852536+prayanaputra@users.noreply.github.com>
2024-06-20 10:10:57 +0700

    Update breast_app.py

--- breast_app.py
+++ breast_app.py
@@ -1,8 +1,6 @@
 import pickle
 import streamlit as st
 from PIL import Image
-import pandas as pd
-from sklearn.metrics import accuracy_score
 
 # Load the model
 model = pickle.load(open('breastcancer.sav', 'rb'))
@@ -14,6 +12,10 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded"
 )
+
+# Load and display an image
+image = Image.open("breast_cancer_image.jpg")  # Ensure the image file is in the same directory or provide the correct path
+st.image(image, use_column_width=True)
 
 # Define the front end interface
 st.markdown(
@@ -31,66 +33,55 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Sidebar for navigation
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Prediction"])
+st.title('Breast Cancer Prediction')
 
-# Prediction Page
-if page == "Prediction":
-    # Load and display an image
-    image = Image.open("breast_cancer_image.jpg")  # Pastikan file gambar tersedia di direktori yang sama atau berikan path yang benar
-    st.image(image, use_column_width=True)
+st.markdown(
+    """
+    ### Please enter the following details:
+    """
+)
 
-    st.title('Breast Cancer Prediction')
+# Layout for input fields
+col1, col2 = st.columns(2)
+
+with col1:
+    clump_thickness = st.number_input('Clump Thickness')
+    cell_size_uniformity = st.number_input('Cell Size Uniformity')
+    cell_shape_uniformity = st.number_input('Cell Shape Uniformity')
+    marginal_adhesion = st.number_input('Marginal Adhesion')
+    single_epi_cell_size = st.number_input('Single Epithelial Cell Size')
+
+with col2:
+    bare_nuclei = st.number_input('Bare Nuclei')
+    bland_chromatin = st.number_input('Bland Chromatin')
+    normal_nucleoli = st.number_input('Normal Nucleoli')
+    mitoses = st.number_input('Mitoses')
+
+# Make predictions
+if st.button('Predict'):
+    input_data = (
+        clump_thickness, cell_size_uniformity, cell_shape_uniformity, 
+        marginal_adhesion, single_epi_cell_size, bare_nuclei, 
+        bland_chromatin, normal_nucleoli, mitoses
+    )
+    prediction = model.predict([input_data])
 
     st.markdown(
         """
-        ### Please enter the following details:
+        ### Prediction Result:
         """
     )
 
-    # Layout for input fields
-    col1, col2 = st.columns(2)
+    if prediction[0] == 0:
+        st.success('Kanker Anda Termasuk Kanker **Benign (Jinak)**.')
+    else:
+        st.error('Kanker Anda Termasuk Kanker **Malignant (Ganas)**.')
 
-    with col1:
-        clump_thickness = st.number_input('Clump Thickness')
-        cell_size_uniformity = st.number_input('Cell Size Uniformity')
-        cell_shape_uniformity = st.number_input('Cell Shape Uniformity')
-        marginal_adhesion = st.number_input('Marginal Adhesion')
-        single_epi_cell_size = st.number_input('Single Epithelial Cell Size')
-
-    with col2:
-        bare_nuclei = st.number_input('Bare Nuclei')
-        bland_chromatin = st.number_input('Bland Chromatin')
-        normal_nucleoli = st.number_input('Normal Nucleoli')
-        mitoses = st.number_input('Mitoses')
-
-    # Make predictions
-    if st.button('Predict'):
-        input_data = (
-            clump_thickness, cell_size_uniformity, cell_shape_uniformity, 
-            marginal_adhesion, single_epi_cell_size, bare_nuclei, 
-            bland_chromatin, normal_nucleoli, mitoses
-        )
-        
-        prediction = model.predict([input_data])
-
-        st.markdown(
-            """
-            ### Prediction Result:
-            """
-        )
-
-        if prediction[0] == 0:
-            st.success('Kanker Anda Termasuk Kanker **Benign (Jinak)**.')
-        else:
-            st.error('Kanker Anda Termasuk Kanker **Malignant (Ganas)**.')
-
-        # Footer
-        st.markdown(
-            """
-            <hr>
-            <p style="text-align:center;">Kelompok 1 - 21S1SI-Machine2(SI163) </p>
-            """,
-            unsafe_allow_html=True
-        )
+# Footer
+st.markdown(
+    """
+    <hr>
+    <p style="text-align:center;">with Kelompok 1 - 21S1SI-Machine2(SI163) </p>
+    """,
+    unsafe_allow_html=True
+)
